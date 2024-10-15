432e76c8eb9a46aa9b707130a54daee61986a37a
Mikhail Dziarkach <m.dziarkach@godeltech.com>
2024-10-15 15:01:49 +0200

    Add first endpoint
    - Add /api/ping endpoint
    - Install ExpressJS
    - Install dotenv
    - Add config file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, world!")
+import express, { Request, Response } from 'express';
+import config from './config';
+
+const app = express();
+
+app.get('/api/ping', (req: Request, res: Response) => {
+    res.json({"message":"pong"});
+})
+
+app.listen(config.port, () => console.log(`Server listening on port ${config.port}`));
