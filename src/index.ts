dae21fb725fca760e55fecb4241d2359ef84a9f0
Mikhail Dziarkach <m.dziarkach@godeltech.com>
2024-11-07 12:26:16 +0100

    Set Up SQLite Database (ORM)
    - Install prisma
    - Add expense schema
    - Refactor exist endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,34 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 import config from './config';
-import Database from 'better-sqlite3';
 
 const app = express();
 
-let db;
-try {
-	db = new Database('dev.db', { verbose: console.log });
-} catch (error) {
-	console.log({ error });
-	process.exit(1);
-}
+const prisma = new PrismaClient();
 
-db.exec(
-	`CREATE TABLE IF NOT EXISTS expenses (
-	id INTEGER,
-	amount REAL,
-	currency TEXT,
-	description TEXT,
-	category TEXT,
-	created_at INTEGER,
-	updated_at INTEGER,
-	PRIMARY KEY(id)
-)`,
-);
-
-app.post('/api/expenses', (req: Request, res: Response) => {
-	const insert = db.prepare(
-		'INSERT INTO expenses (amount, currency, description, category, created_at, updated_at) VALUES (@amount, @currency, @description, @category, @created_at, @updated_at)',
-	);
-	const insertMany = db.transaction((cats) => {
-		for (const cat of cats) insert.run(cat);
-	});
-
-	insertMany([
+app.post('/api/expenses', async (req: Request, res: Response) => {
+	const expenses = [
 		{
 			amount: 100,
 			currency: 'USD',
 			description: 'Bar fee',
 			category: 'category 1',
-			created_at: Date.now(),
-			updated_at: Date.now(),
 		},
 		{
 			amount: 10,
 			currency: 'EUR',
 			description: 'Football fee',
 			category: 'category 2',
-			created_at: Date.now(),
-			updated_at: Date.now(),
 		},
-	]);
+	];
+
+	await prisma.expenses.createMany({ data: expenses });
 
 	res.send('ok');
 });
 
-app.get('/api/expenses', (req: Request, res: Response) => {
-	const result = db.prepare('SELECT * FROM expenses').all();
+app.get('/api/expenses', async (req: Request, res: Response) => {
+	const result = await prisma.expenses.findMany();
 
 	res.send(result);
 });
