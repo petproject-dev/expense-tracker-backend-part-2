db15942325093be9bbcabbd61079a4acdfd1b0ef
Mikhail Dziarkach <m.dziarkach@godeltech.com>
2024-12-18 18:47:10 +0100

    task-3: Update formatting rules, run formatter

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    port: process.env.PORT || 8080
-}
+	port: process.env.PORT || 8080,
+	NODE_ENV: process.env.NODE_ENV || 'development',
+};
