92380c42caef370665a4ef820ec092608bb467d6
Mikhail Dziarkach <m.dziarkach@godeltech.com>
2024-12-19 16:10:27 +0100

    Task 6: Add validator, add error handler, add Exception class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
 import config from './config';
-import Database from 'better-sqlite3';
+import errorHandler from './middlewares/errorHandler';
+import validator from './middlewares/validator';
+import createExpenseSchema from './validatorSchemas/createExpenseSchema';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,7 @@ const prisma = new PrismaClient();
 
 app.post(
 	'/api/expenses',
+	validator(createExpenseSchema),
 	async (req: Request, res: Response, next: NextFunction) => {
 		try {
 			const data = req.body;
@@ -35,59 +38,13 @@ app.get(
 	},
 );
 
-const db = new Database('dev.db', { verbose: console.log });
-
-db.exec(
-	`CREATE TABLE IF NOT EXISTS expenses (
-	id INTEGER,
-	amount INTEGER,
-	currency TEXT,
-	description TEXT,
-	category TEXT,
-	created_at INTEGER,
-	updated_at INTEGER,
-	PRIMARY KEY(id)
-)`,
-);
-
-app.post('/api/expenses', (req: Request, res: Response) => {
-	const insert = db.prepare(
-		'INSERT INTO expenses (amount, currency, description, category, created_at, updated_at) VALUES (@amount, @currency, @description, @category, @created_at, @updated_at)',
-	);
-	const insertMany = db.transaction((cats) => {
-		for (const cat of cats) insert.run(cat);
-	});
-
-	insertMany([
-		{
-			amount: 100,
-			currency: 'USD',
-			description: 'Bar fee',
-			category: 'category 1',
-			created_at: Date.now(),
-			updated_at: Date.now(),
-		},
-		{
-			amount: 10,
-			currency: 'EUR',
-			description: 'Football fee',
-			category: 'category 2',
-			created_at: Date.now(),
-			updated_at: Date.now(),
-		},
-	]);
-
-	res.send('ok');
-});
-
-app.get('/api/expenses', (req: Request, res: Response) => {
-	const result = db.prepare('SELECT * FROM expenses').all();
-
-	res.send(result);
-});
-
 app.get('/api/ping', (req: Request, res: Response) => {
 	res.json({ message: 'pong' });
+});
+
+app.use(errorHandler);
+app.use('*', (req: Request, res: Response) => {
+	res.status(404).send('Page not found');
 });
 
 app.listen(config.port, () =>
